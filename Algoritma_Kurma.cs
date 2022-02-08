82c2752a9ead78cf7eba56212f3e04ad84470a09
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-08 20:28:43 +0300

    Algoritma Kurma sınıfına 2 adet metot eklendi, henüz test edilmedi.

--- Algoritma_Kurma.cs
+++ Algoritma_Kurma.cs
@@ -3,6 +3,30 @@ namespace Algoritma
 {
     public class Algoritma_Kurma
     {
-        
+        public int[] SayılarıKaydetme(string[] arr)
+        {
+            int[] s =new int[arr.Length/2];
+            int counter=0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+                if(i%2!=0)
+                {
+                    s[counter]= int.Parse(arr[i]);
+                }
+            }
+            return s;
+        }
+        public string[] GirilenMetniSilme(string[] arr, int[]sayılar)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i%2==0)
+                {
+                    arr[i].Remove(sayılar[i],sayılar[i+1]);
+                }
+            }
+            return arr;
+        }
     }
 }
